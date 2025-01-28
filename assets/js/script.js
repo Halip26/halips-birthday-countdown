cf0d13a43ca7ef56e37a90a77ed8c6636ec4b145
Halip26 <77138807+Halip26@users.noreply.github.com>
2025-01-29 03:01:49 +0800

    update future birthday date to January 26, 2026

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 
 // Set the date of my birthday
-const futureBirthday = new Date(2025, 0, 26, 23, 59, 0);
+const futureBirthday = new Date(2026, 0, 26, 23, 59, 0);
 
 const year = futureBirthday.getFullYear();
 const hours = futureBirthday.getHours();
